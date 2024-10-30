25c3704df413a0b1cbb294d09415d9bdde207aec
Sergio Alexandre <sergioalexandre0716@gmail.com>
2024-10-30 18:51:34 -0300

    add initial style variables and setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import "./App.scss";
+import "./assets/styles/defaults.scss";
 
 function App() {
   return (
-    <>
+    <div className="">
       <h1 className="hello-scss">Hello World</h1>
-    </>
+    </div>
   );
 }
 
