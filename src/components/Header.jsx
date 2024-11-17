727703625807861310cccaa1833db38c968454cf
Pedro de Oliveira Machado <pedromachado.2003@alunos.utfpr.edu.br>
2024-11-17 16:15:33 -0300

    fix header and mobile nav

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,10 +2,10 @@ const Header = ({ setAction }) => {
   return (
     <>
       <header className="unlogged-header">
-        <a href="#main-nav" id="menu">
-          <i className="fi fi-rr-menu-burger" src="" alt="" />
-        </a>
         <div className="content">
+          <a href="#main-nav" id="menu">
+            <i className="fi fi-rr-menu-burger" src="" alt="" />
+          </a>
           <img src="../../public/images/logo-white.png" alt="logo do projeto" />
           <nav>
             <a href="#about">Sobre nós</a>
@@ -13,7 +13,7 @@ const Header = ({ setAction }) => {
             <a href="#contact">Contato</a>
           </nav>
           <div className="action">
-            <button
+            <a
               className="button"
               onClick={(e) => {
                 e.preventDefault(); // Previne comportamento padrão de redirecionamento
@@ -21,10 +21,35 @@ const Header = ({ setAction }) => {
               }}
             >
               Entrar <i className="fi fi-rr-arrow-small-right"></i>
-            </button>
+            </a>
           </div>
         </div>
       </header>
+      <div id="main-nav">
+        <div className="inset">
+          <a href="#close" className="close">
+            <i className="fi fi-rr-circle-xmark"></i>
+          </a>
+          <a href="/" className="logo">
+            <img src="../../public/images/favicon-white.png" alt="" />
+          </a>
+          <nav>
+            <a href="./">Sobre nós</a>
+            <a href="#quem-somos">Doe</a>
+            <a href="#contato">Contato</a>
+          </nav>
+          <div className="action">
+            <a href="#open-login" className="button alt"
+              onClick={(e) => {
+                e.preventDefault(); // Previne comportamento padrão de redirecionamento
+                setAction("login-modal"); // Mostra o modal
+              }}
+            >
+              entrar <i className="fi fi-rr-arrow-small-right"></i>
+            </a>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
