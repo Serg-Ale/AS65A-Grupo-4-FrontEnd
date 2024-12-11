d39eae33c9374c044f7114e63c6c97a857793197
Pedro de Oliveira Machado <pedromachado.2003@alunos.utfpr.edu.br>
2024-12-10 22:35:51 -0300

    created: dashboard for administration

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -71,7 +71,7 @@ const Dashboard = () => {
             </div>
             <span className="copyright">© Copyright Bons Fluidos - 2024</span>
           </aside>
-          <div id="storage" className="main-container">
+          {/* <div id="storage" className="main-container">
             <div className="title">
               <h2>Controle o estoque</h2>
               <p>
@@ -166,6 +166,147 @@ const Dashboard = () => {
                   <p>categoria</p>
                 </div>
               </button>
+            </div>
+          </div> */}
+
+          <div id="administration" className="main-container">
+            <div className="title">
+              <h2>Controle de pessoal</h2>
+              <p>Adicione e gerencie os usuários da plataforma.</p>
+            </div>
+            <div className="user-list">
+              <div className="item">
+                <div className="start">
+                  <div className="icon">
+                    <i className="fi fi-rr-circle-user"></i>
+                  </div>
+                  <div className="info">
+                    <h3>Padra Marcela Corsa</h3>
+                    <div className="password">
+                      <span>*********</span>
+                      <button>
+                        <i id="eye-opened" className="fi fi-rr-eye"></i>
+                        {/* <i id="eye-closed" className="fi fi-rr-eye-crossed"></i> */}
+                      </button>
+                    </div>
+                  </div>
+                </div>
+                <div className="end">
+                  <button>
+                    <i className="fi fi-rr-pen-field"></i>
+                  </button>
+                </div>
+              </div>
+              <div className="item">
+                <div className="start">
+                  <div className="icon">
+                    <i className="fi fi-rr-circle-user"></i>
+                  </div>
+                  <div className="info">
+                    <h3>Padra Marcela Corsa</h3>
+                    <div className="password">
+                      <span>*********</span>
+                      <button>
+                        <i id="eye-opened" className="fi fi-rr-eye"></i>
+                        {/* <i id="eye-closed" className="fi fi-rr-eye-crossed"></i> */}
+                      </button>
+                    </div>
+                  </div>
+                </div>
+                <div className="end">
+                  <button>
+                    <i className="fi fi-rr-pen-field"></i>
+                  </button>
+                </div>
+              </div>
+              <div className="item">
+                <div className="start">
+                  <div className="icon">
+                    <i className="fi fi-rr-circle-user"></i>
+                  </div>
+                  <div className="info">
+                    <h3>Padra Marcela Corsa</h3>
+                    <div className="password">
+                      <span>*********</span>
+                      <button>
+                        <i id="eye-opened" className="fi fi-rr-eye"></i>
+                        {/* <i id="eye-closed" className="fi fi-rr-eye-crossed"></i> */}
+                      </button>
+                    </div>
+                  </div>
+                </div>
+                <div className="end">
+                  <button>
+                    <i className="fi fi-rr-pen-field"></i>
+                  </button>
+                </div>
+              </div>
+              <div className="item">
+                <div className="start">
+                  <div className="icon">
+                    <i className="fi fi-rr-circle-user"></i>
+                  </div>
+                  <div className="info">
+                    <h3>Padra Marcela Corsa</h3>
+                    <div className="password">
+                      <span>*********</span>
+                      <button>
+                        <i id="eye-opened" className="fi fi-rr-eye"></i>
+                        {/* <i id="eye-closed" className="fi fi-rr-eye-crossed"></i> */}
+                      </button>
+                    </div>
+                  </div>
+                </div>
+                <div className="end">
+                  <button>
+                    <i className="fi fi-rr-pen-field"></i>
+                  </button>
+                </div>
+              </div>
+              <div className="item">
+                <div className="start">
+                  <div className="icon">
+                    <i className="fi fi-rr-circle-user"></i>
+                  </div>
+                  <div className="info">
+                    <h3>Padra Marcela Corsa</h3>
+                    <div className="password">
+                      <span>*********</span>
+                      <button>
+                        <i id="eye-opened" className="fi fi-rr-eye"></i>
+                        {/* <i id="eye-closed" className="fi fi-rr-eye-crossed"></i> */}
+                      </button>
+                    </div>
+                  </div>
+                </div>
+                <div className="end">
+                  <button>
+                    <i className="fi fi-rr-pen-field"></i>
+                  </button>
+                </div>
+              </div>
+              <div className="item">
+                <div className="start">
+                  <div className="icon">
+                    <i className="fi fi-rr-circle-user"></i>
+                  </div>
+                  <div className="info">
+                    <h3>Padra Marcela Corsa</h3>
+                    <div className="password">
+                      <span>*********</span>
+                      <button>
+                        <i id="eye-opened" className="fi fi-rr-eye"></i>
+                        {/* <i id="eye-closed" className="fi fi-rr-eye-crossed"></i> */}
+                      </button>
+                    </div>
+                  </div>
+                </div>
+                <div className="end">
+                  <button>
+                    <i className="fi fi-rr-pen-field"></i>
+                  </button>
+                </div>
+              </div>
             </div>
           </div>
         </div>
