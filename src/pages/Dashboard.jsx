b7ed49016ed1272b9ed1f99b81c356ecf0222ddd
Pedro de Oliveira Machado <pedromachado.2003@alunos.utfpr.edu.br>
2024-12-10 21:53:39 -0300

    created: Dashboard + Storage container

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,8 +1,11 @@
 const Dashboard = () => {
   return (
-    <>
+    <main className="dashboard">
       <header>
         <div className="content">
+          <div className="logo">
+            <img src="./public/images/logo-white.png" alt="" />
+          </div>
           <div className="title">
             <h1>Dashboard</h1>
           </div>
@@ -16,10 +19,14 @@ const Dashboard = () => {
               <div className="user-nav">
                 <ul>
                   <li>
-                    <a href="https://api.whatsapp.com">Suporte <i className="fi fi-rr-phone-flip"></i></a>
+                    <a href="https://api.whatsapp.com">
+                      Suporte <i className="fi fi-rr-phone-flip"></i>
+                    </a>
                   </li>
                   <li>
-                    <a href="">Sair <i className="fi fi-rr-exit"></i></a>
+                    <a href="">
+                      Sair <i className="fi fi-rr-exit"></i>
+                    </a>
                   </li>
                 </ul>
               </div>
@@ -27,54 +34,143 @@ const Dashboard = () => {
           </div>
         </div>
       </header>
-      <aside>
-        <div className="aside-box">
-          <div className="logo">
-            <img src="../../public/images/logo.png" alt="" />
+      <div className="board">
+        <div className="content">
+          <aside>
+            <div className="list">
+              <button className="item">
+                <div className="title">
+                  <h3>Estoque</h3>
+                  <span>
+                    Etiam ac volutpat nisi. Aenean vitae ullamcorper ligula.
+                    Donec quis consectetur nibh.
+                  </span>
+                </div>
+                <i className="fi fi-rr-box-circle-check"></i>
+              </button>
+              <button className="item">
+                <div className="title">
+                  <h3>Movimentações</h3>
+                  <span>
+                    Etiam ac volutpat nisi. Aenean vitae ullamcorper ligula.
+                    Donec quis consectetur nibh.
+                  </span>
+                </div>
+                <i className="fi fi-rr-exchange"></i>
+              </button>
+              <button className="item">
+                <div className="title">
+                  <h3>Administração</h3>
+                  <span>
+                    Etiam ac volutpat nisi. Aenean vitae ullamcorper ligula.
+                    Donec quis consectetur nibh.
+                  </span>
+                </div>
+                <i className="fi fi-rr-user-gear"></i>
+              </button>
+            </div>
+            <span className="copyright">© Copyright Bons Fluidos - 2024</span>
+          </aside>
+          <div id="storage" className="main-container">
+            <div className="title">
+              <h2>Controle o estoque</h2>
+              <p>
+                Adicione produtos, modifique suas especificações e vizualize em
+                tempo real.
+              </p>
+            </div>
+            <div className="products-list">
+              <button className="item create">
+                <div className="icon">
+                  <i className="fi fi-rr-add"></i>
+                </div>
+                <h3>Adicionar Produto</h3>
+              </button>
+              <button className="item">
+                <div className="item-counter">
+                  <strong>45</strong>
+                </div>
+                <div className="image">
+                  <img
+                    src="../../public/images/placeholder.png"
+                    alt="bla bla bla"
+                  />
+                </div>
+                <h3>nome do produto</h3>
+                <div className="info">
+                  <span>Clique para modificar...</span>
+                  <p>categoria</p>
+                </div>
+              </button>
+              <button className="item">
+                <div className="item-counter">
+                  <strong>45</strong>
+                </div>
+                <div className="image">
+                  <img
+                    src="../../public/images/placeholder.png"
+                    alt="bla bla bla"
+                  />
+                </div>
+                <h3>nome do produto</h3>
+                <div className="info">
+                  <span>Clique para modificar...</span>
+                  <p>categoria</p>
+                </div>
+              </button>
+              <button className="item">
+                <div className="item-counter">
+                  <strong>45</strong>
+                </div>
+                <div className="image">
+                  <img
+                    src="../../public/images/placeholder.png"
+                    alt="bla bla bla"
+                  />
+                </div>
+                <h3>nome do produto</h3>
+                <div className="info">
+                  <span>Clique para modificar...</span>
+                  <p>categoria</p>
+                </div>
+              </button>
+              <button className="item">
+                <div className="item-counter">
+                  <strong>45</strong>
+                </div>
+                <div className="image">
+                  <img
+                    src="../../public/images/placeholder.png"
+                    alt="bla bla bla"
+                  />
+                </div>
+                <h3>nome do produto</h3>
+                <div className="info">
+                  <span>Clique para modificar...</span>
+                  <p>categoria</p>
+                </div>
+              </button>
+              <button className="item">
+                <div className="item-counter">
+                  <strong>45</strong>
+                </div>
+                <div className="image">
+                  <img
+                    src="../../public/images/placeholder.png"
+                    alt="bla bla bla"
+                  />
+                </div>
+                <h3>nome do produto</h3>
+                <div className="info">
+                  <span>Clique para modificar...</span>
+                  <p>categoria</p>
+                </div>
+              </button>
+            </div>
           </div>
-          <nav>
-            <div>
-              <strong>Início</strong>{" "}
-              <i className="fi fi-rr-angle-small-up"></i>
-              <ul>
-                <li>Sub grupo</li>
-                <li>Sub grupo</li>
-                <li>Sub grupo</li>
-              </ul>
-            </div>
-            <siv>
-              <strong>Estoque</strong>{" "}
-              <i className="fi fi-rr-angle-small-up"></i>
-              <ul>
-                <li>Sub grupo</li>
-                <li>Sub grupo</li>
-                <li>Sub grupo</li>
-              </ul>
-            </siv>
-            <div>
-              <strong>Movimentações</strong>{" "}
-              <i className="fi fi-rr-angle-small-up"></i>
-              <ul>
-                <li>Sub grupo</li>
-                <li>Sub grupo</li>
-                <li>Sub grupo</li>
-              </ul>
-            </div>
-            <div>
-              <strong>Administração</strong>{" "}
-              <i className="fi fi-rr-angle-small-up"></i>
-              <ul>
-                <li>Sub grupo</li>
-                <li>Sub grupo</li>
-                <li>Sub grupo</li>
-              </ul>
-            </div>
-          </nav>
         </div>
-        <span>© Copyright Bons Fluidos - 2024</span>
-      </aside>
-      <main></main>
-    </>
+      </div>
+    </main>
   );
 };
 
