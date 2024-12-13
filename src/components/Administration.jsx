24a24e88588051e66ba24ee51439e1a0bf6b8670
Pedro de Oliveira Machado <pedromachado.2003@alunos.utfpr.edu.br>
2024-12-13 19:16:55 -0300

    create: nem user button added for admins

--- src/components/Administration.jsx
+++ src/components/Administration.jsx
@@ -1,23 +1,35 @@
-import AdministrationItem from './AdministrationItem.jsx';
+import AdministrationItem from "./AdministrationItem.jsx";
 
 const Administration = () => {
   return (
-
-
-
     <div id="administration" className="main-container">
       <div className="title">
         <h2>Controle de pessoal</h2>
         <p>Adicione e gerencie os usuários da plataforma.</p>
       </div>
       <div className="user-list">
+        <div className="item create">
+          <div className="start">
+            <div className="icon">
+              <i className="fi fi-rr-circle-user"></i>
+            </div>
+            <div className="info">
+              <h3>Novo usuário...</h3>
+            </div>
+          </div>
+          <div className="end">
+            <button>
+              <i className="fi fi-rr-add"></i>
+            </button>
+          </div>
+        </div>
         <AdministrationItem />
         <AdministrationItem />
         <AdministrationItem />
         <AdministrationItem />
         <AdministrationItem />
       </div>
-    </div >
+    </div>
   );
 };
 
