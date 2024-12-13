3302004c4dae0fbc56b7190d408c94bf1a565931
Serg-Ale <sergioalexandre0716@gmail.com>
2024-12-13 16:12:21 -0300

    Modularize Aside and MainHeader into components

---
+++ src/components/Aside.jsx
@@ -0,0 +1,42 @@
+const Aside = () => {
+  return (
+
+    <aside>
+      <div className="list">
+        <button className="item">
+          <div className="title">
+            <h3>Estoque</h3>
+            <span>
+              Etiam ac volutpat nisi. Aenean vitae ullamcorper ligula.
+              Donec quis consectetur nibh.
+            </span>
+          </div>
+          <i className="fi fi-rr-box-circle-check"></i>
+        </button>
+        <button className="item">
+          <div className="title">
+            <h3>Movimentações</h3>
+            <span>
+              Etiam ac volutpat nisi. Aenean vitae ullamcorper ligula.
+              Donec quis consectetur nibh.
+            </span>
+          </div>
+          <i className="fi fi-rr-exchange"></i>
+        </button>
+        <button className="item">
+          <div className="title">
+            <h3>Administração</h3>
+            <span>
+              Etiam ac volutpat nisi. Aenean vitae ullamcorper ligula.
+              Donec quis consectetur nibh.
+            </span>
+          </div>
+          <i className="fi fi-rr-user-gear"></i>
+        </button>
+      </div>
+      <span className="copyright">© Copyright Bons Fluidos - 2024</span>
+    </aside>
+  );
+};
+
+export default Aside;
---
+++ src/components/MainHeader.jsx
@@ -0,0 +1,40 @@
+const MainHeader = () => {
+  return (
+
+    <header>
+      <div className="content">
+        <div className="logo">
+          <img src="./public/images/logo-white.png" alt="" />
+        </div>
+        <div className="title">
+          <h1>Dashboard</h1>
+        </div>
+        <div className="action">
+          <div className="button">
+            <span>Usuário</span>
+            <div className="user-icon">
+              <i className="fi fi-rr-user"></i>
+            </div>
+            <i id="arrow-animate" className="fi fi-rr-angle-small-up"></i>
+            <div className="user-nav">
+              <ul>
+                <li>
+                  <a href="https://api.whatsapp.com">
+                    Suporte <i className="fi fi-rr-phone-flip"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="">
+                    Sair <i className="fi fi-rr-exit"></i>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default MainHeader;
